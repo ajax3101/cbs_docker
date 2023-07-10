b2678104a6ee6e67df3de7a3bcbab25cd999b4ea
Denys Morozov <ajax3101@gmail.com>
2023-07-10 16:17:44 +0300

    change bot lesson 2

--- bot/src/settings.py
+++ bot/src/settings.py
@@ -8,3 +8,6 @@ load_dotenv()
 
 try:
     BOT_TOKEN = os.environ["BOT_TOKEN"]
+except KeyError as err:
+    logging.critical(f"Can`t read tocken from enviroment variable. Message: {err}")
+    raise KeyError(err)
